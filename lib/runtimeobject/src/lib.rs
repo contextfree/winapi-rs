a46f9de185f405c7537a685077c29c16ecd6a286
Max Strini <Max Strini>
2016-03-22 18:58:34 -0700

    Add trailing commas to last arguments of function declarations

--- lib/runtimeobject/src/lib.rs
+++ lib/runtimeobject/src/lib.rs
@@ -51,18 +51,18 @@ extern "system" {
     // pub fn WindowsCompareStringOrdinal();
     // pub fn WindowsConcatString();
     pub fn WindowsCreateString(
-        sourceString: PCNZWCH, length: UINT32, string: *mut HSTRING
+        sourceString: PCNZWCH, length: UINT32, string: *mut HSTRING,
     ) -> HRESULT;
     pub fn WindowsCreateStringReference(
         sourceString: PCWSTR, length: UINT32,
-        hstringHeader: *mut HSTRING_HEADER, string: *mut HSTRING
+        hstringHeader: *mut HSTRING_HEADER, string: *mut HSTRING,
     ) -> HRESULT; 
     // pub fn WindowsDeleteString();
     // pub fn WindowsDeleteStringBuffer();
     // pub fn WindowsDuplicateString();
     // pub fn WindowsGetStringLen();
     pub fn WindowsGetStringRawBuffer(
-        string: HSTRING, length: *mut UINT32
+        string: HSTRING, length: *mut UINT32,
     ) -> PCWSTR;
     // pub fn WindowsInspectString();
     // pub fn WindowsIsStringEmpty();
